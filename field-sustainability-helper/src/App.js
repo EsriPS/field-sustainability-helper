05acaa7409bb838c38e4c08515ffaf71426a1568
Paige Henderson <phenderson@esri.com>
2022-01-12 17:02:01 -0600

    User can draw temp geometries on the map

--- field-sustainability-helper/src/App.js
+++ field-sustainability-helper/src/App.js
@@ -2,8 +2,19 @@ import "./App.css";
 import "@arcgis/core/assets/esri/themes/light/main.css";
 import Header from "./components/Header";
 import EsriMap from "./components/EsriMap";
+import InputPane from "./components/InputPane";
+
+import React, { useEffect, useState, useRef } from "react";
+
 
 function App() {
+  const [view, setView] = useState(null);
+
+
+  function setMapView(mapView) {
+    setView(mapView);
+  }
+
   return (
     <div className="App">
       <Header
@@ -11,7 +22,10 @@ function App() {
         backgroundColor="#212121"
         title="FiSH (Field Sustainability Helper)"
       ></Header>
-      <EsriMap webMapId={"e691172598f04ea8881cd2a4adaa45ba"}></EsriMap>
+      <section className="container">
+        <InputPane label="Draw Field Boundary" view={view}></InputPane>
+        <EsriMap webMapId={"e691172598f04ea8881cd2a4adaa45ba"} setView={setMapView}></EsriMap>
+      </section>
     </div>
   );
 }
