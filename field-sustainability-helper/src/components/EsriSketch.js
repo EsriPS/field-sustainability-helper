b6db4d2ebff9f16195a577a843c4234bbf8c68ae
Gavin Rehkemper <grehkemper@esri.com>
2022-01-13 13:56:04 -0600

    fix duplicate sketch widget issue

--- field-sustainability-helper/src/components/EsriSketch.js
+++ field-sustainability-helper/src/components/EsriSketch.js
@@ -11,10 +11,15 @@ function EsriSketch({ label, view, onSketchResultGraphic }) {
         (x) => x.id === "temp_graphics_layer"
       )[0];
 
+      // Esri widgets REPLACE the DOM node we sent in, so we need to create a
+      // "parent" DOM node so we can properly destroy the contents on the unload.
+      const domNode = document.createElement("div");
+      sketchRef.current.appendChild(domNode);
+
       const sketchWidget = new Sketch({
         view: view,
         layer: gLayer,
-        container: sketchRef.current,
+        container: domNode,
         visibleElements: {
           createTools: {
             point: false,
@@ -30,13 +35,21 @@ function EsriSketch({ label, view, onSketchResultGraphic }) {
       });
 
       // Listen to sketch widget's create event.
-      sketchWidget.on("create", function (evt) {
+      const watchHandle = sketchWidget.on("create", function (evt) {
         // check if the create event's state has changed to complete indicating
         // the graphic create operation is completed.
         if (evt.state === "complete") {
           onSketchResultGraphic(evt.graphic);
         }
       });
+
+      return () => {
+        // Cleanup the widget and any event handles
+        if (sketchWidget && watchHandle) {
+          watchHandle.remove();
+          sketchWidget.destroy();
+        }
+      };
     }
   }, [view]);
 
